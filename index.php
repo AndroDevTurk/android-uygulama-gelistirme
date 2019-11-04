aa161d13a9370d400887f5705879ae602eb30dcc
AndroDevTurk <AndroDevTurk@users.noreply.github.com>
2019-11-04 19:11:42 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Android Uygulama Geliştirme</h1>
+<h5></h5>
+<h2>Bu Bir AndroDevTürk Hizmetidir!</h2>
+<?php
+echo "app-ads.txt dosyası nasıl eklenir.
+Bu konuyu sizinle paylaşmak istiyorum. Umarım Memnun kalırsınız. 
+";
+?>
+
+</body>
+</html>
